cefe108289004f44decee9cb61273b0f6cb40191
Tomasz Śmiechowicz <tomasz.smiechowicz@urtica.pl>
2017-10-24 09:25:24 +0200

    find by group of cards

--- src/main/java/com/tsmiecho/paymentor/service/IssuerIdentificationNumberRangeService.java
+++ src/main/java/com/tsmiecho/paymentor/service/IssuerIdentificationNumberRangeService.java
@@ -6,13 +6,21 @@ import com.tsmiecho.paymentor.model.Bank;
 import com.tsmiecho.paymentor.model.IssuerIdentificationNumber;
 import com.tsmiecho.paymentor.model.PrimaryAccountNumber;
 import com.tsmiecho.paymentor.repository.IssuerIdentificationNumberRangeRepository;
+import lombok.extern.log4j.Log4j;
+import org.javatuples.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * Created by Tomasz Śmiechowicz on 20.10.17.
  */
 @Service
+@Log4j
 public class IssuerIdentificationNumberRangeService {
 
     private final IssuerIdentificationNumberRangeRepository repository;
@@ -25,13 +33,69 @@ public class IssuerIdentificationNumberRangeService {
         this.bankLifecycleService = bankLifecycleService;
     }
 
-    public Bank findBank(PrimaryAccountNumber pan) throws NoSuchBankException, TechnicalException {
+    public Bank findBank(PrimaryAccountNumber pan) throws NoSuchBankException, TechnicalException, IllegalAccessException {
+        if(pan == null){
+            throw new IllegalArgumentException("Primary account number cannot be null");
+        }
         return findBank(pan.getIssuerIdentificationNumber());
     }
 
-    public Bank findBank(IssuerIdentificationNumber inn) throws NoSuchBankException, TechnicalException {
+    public Bank findBank(IssuerIdentificationNumber inn) throws NoSuchBankException, TechnicalException, IllegalAccessException {
+        if(inn == null){
+            throw new IllegalArgumentException("Issuer identification number cannot be null");
+        }
         final Bank bank = repository.findByIssuerIdentificationNumber(inn);
         return new Bank(bank.getId(), bank.getName(), bankLifecycleService.isBankSuspended(String.valueOf(bank.getId())));
-
     }
+
+    public List<Pair<PrimaryAccountNumber, Bank>> findBanksByPrimaryAccountNumbers(List<PrimaryAccountNumber> pans) throws NoSuchBankException, TechnicalException, IllegalAccessException {
+        if(CollectionUtils.isEmpty(pans)){
+            throw new IllegalArgumentException("List of primary account numbers cannot be empty");
+        }
+        return pans.stream()
+                .map(getPrimaryAccountNumberPairFunction())
+                .collect(Collectors.toList());
+    }
+
+    public List<Pair<IssuerIdentificationNumber, Bank>> findBanksByIssuerIdentificationNumbers(List<IssuerIdentificationNumber> inns) throws NoSuchBankException, TechnicalException, IllegalAccessException {
+        if(CollectionUtils.isEmpty(inns)){
+            throw new IllegalArgumentException("List of issuer identification numbers cannot be empty");
+        }
+        return inns.stream()
+                .map(getIssuerIdentificationNumberPairFunction())
+                .collect(Collectors.toList());
+    }
+
+    private Function<PrimaryAccountNumber, Pair<PrimaryAccountNumber, Bank>> getPrimaryAccountNumberPairFunction() {
+        return pan -> {
+            Bank bankFromRepository;
+            try {
+                bankFromRepository = repository.findByIssuerIdentificationNumber(pan.getIssuerIdentificationNumber());
+                final boolean isBankSuspended = bankLifecycleService.isBankSuspended(String.valueOf(bankFromRepository.getId()));
+                return new Pair<>(pan, new Bank(bankFromRepository.getId(), bankFromRepository.getName(), isBankSuspended));
+            } catch (NoSuchBankException | TechnicalException e) {
+                if (log.isWarnEnabled()) {
+                    log.warn(e);
+                }
+                return new Pair<>(pan, null);
+            }
+        };
+    }
+
+    private Function<IssuerIdentificationNumber, Pair<IssuerIdentificationNumber, Bank>> getIssuerIdentificationNumberPairFunction() {
+        return inn -> {
+            Bank bankFromRepository;
+            try {
+                bankFromRepository = repository.findByIssuerIdentificationNumber(inn);
+                final boolean isBankSuspended = bankLifecycleService.isBankSuspended(String.valueOf(bankFromRepository.getId()));
+                return new Pair<>(inn, new Bank(bankFromRepository.getId(), bankFromRepository.getName(), isBankSuspended));
+            } catch (NoSuchBankException | TechnicalException e) {
+                if (log.isWarnEnabled()) {
+                    log.warn(e);
+                }
+                return new Pair<>(inn, null);
+            }
+        };
+    }
+
 }
--- src/test/java/com/tsmiecho/paymentor/BankDeterminationTest.java
+++ src/test/java/com/tsmiecho/paymentor/BankDeterminationTest.java
@@ -6,9 +6,15 @@ import com.tsmiecho.paymentor.model.PrimaryAccountNumber;
 import com.tsmiecho.paymentor.repository.IssuerIdentificationNumberRangeRepository;
 import com.tsmiecho.paymentor.service.BankLifecycleService;
 import com.tsmiecho.paymentor.service.IssuerIdentificationNumberRangeService;
+import org.javatuples.Pair;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,16 +25,13 @@ public class BankDeterminationTest {
 
     private IssuerIdentificationNumberRangeService service;
 
-
     private Bank mockedBank = new Bank(2L, "nBank");
 
     @Before
     public void setUp() throws Exception {
-        final IssuerIdentificationNumberRangeRepository mockedRepository = Mockito.mock(IssuerIdentificationNumberRangeRepository.class);
-        Mockito.when(mockedRepository.findByIssuerIdentificationNumber(Mockito.anyObject())).thenReturn(mockedBank);
         final BankLifecycleService bankLifecycleService = Mockito.mock(BankLifecycleService.class);
         Mockito.when(bankLifecycleService.isBankSuspended(Mockito.anyString())).thenReturn(false);
-        service = new IssuerIdentificationNumberRangeService(mockedRepository, bankLifecycleService);
+        service = new IssuerIdentificationNumberRangeService(new IssuerIdentificationNumberRangeRepository(), bankLifecycleService);
     }
 
     @Test
@@ -45,5 +48,75 @@ public class BankDeterminationTest {
         final Bank bank = service.findBank(iin);
 
         assertThat(bank).isEqualToComparingFieldByField(mockedBank);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void exceptionWillBeThrownIfIssuerIdentificationNumberWillBeNull() throws Exception {
+        IssuerIdentificationNumber inn = null;
+        service.findBank(inn);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void exceptionWillBeThrownIfPrimaryAccountNumberWillBeNull() throws Exception {
+        PrimaryAccountNumber pan = null;
+        service.findBank(pan);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void exceptionWillBeThrownIfPrimaryAccountNumbersWillBeEmpty() throws Exception {
+        List<PrimaryAccountNumber> pans = null;
+        service.findBanksByPrimaryAccountNumbers(pans);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void exceptionWillBeThrownIfIssuerIdentificationNumbersWillBeEmpty() throws Exception {
+        List<IssuerIdentificationNumber> inns = Collections.emptyList();
+        service.findBanksByIssuerIdentificationNumbers(inns);
+    }
+
+    @Test
+    public void primaryAccountNumberPairsWillBeFound() throws Exception {
+        final PrimaryAccountNumber pan = new PrimaryAccountNumber("6017123456789113", 6);
+        final PrimaryAccountNumber pan2 = new PrimaryAccountNumber("5300123456789113", 4);
+        final List<Pair<PrimaryAccountNumber, Bank>> pairs = service.findBanksByPrimaryAccountNumbers(Arrays.asList(pan, pan2));
+        Assert.assertTrue(pairs != null);
+        Assert.assertTrue(pairs.size() == 2);
+        Assert.assertEquals("601712", pairs.get(0).getValue0().getIssuerIdentificationNumber().getNumber());
+        Assert.assertEquals("5300", pairs.get(1).getValue0().getIssuerIdentificationNumber().getNumber());
+        Assert.assertEquals("Kakao SA", pairs.get(0).getValue1().getName());
+        Assert.assertEquals("OMG Bank Łódzki", pairs.get(1).getValue1().getName());
+    }
+
+    @Test
+    public void primaryAccountNumberPairsWillNotBeFound() throws Exception {
+        final PrimaryAccountNumber pan = new PrimaryAccountNumber("1017123456789113", 6);
+        final List<Pair<PrimaryAccountNumber, Bank>> pairs = service.findBanksByPrimaryAccountNumbers(Collections.singletonList(pan));
+        Assert.assertTrue(pairs != null);
+        Assert.assertTrue(pairs.size() == 1);
+        Assert.assertEquals("101712", pairs.get(0).getValue0().getIssuerIdentificationNumber().getNumber());
+        Assert.assertTrue(pairs.get(0).getValue1() == null);
+    }
+
+    @Test
+    public void issuerIdentificationNumberPairsWillBeFound() throws Exception {
+        final IssuerIdentificationNumber inn = new IssuerIdentificationNumber("601712");
+        final IssuerIdentificationNumber inn2 = new IssuerIdentificationNumber("5300");
+        final List<Pair<IssuerIdentificationNumber, Bank>> pairs = service.findBanksByIssuerIdentificationNumbers(Arrays.asList(inn, inn2));
+        Assert.assertTrue(pairs != null);
+        Assert.assertTrue(pairs.size() == 2);
+        Assert.assertEquals("601712", pairs.get(0).getValue0().getNumber());
+        Assert.assertEquals("5300", pairs.get(1).getValue0().getNumber());
+        Assert.assertEquals("Kakao SA", pairs.get(0).getValue1().getName());
+        Assert.assertEquals("OMG Bank Łódzki", pairs.get(1).getValue1().getName());
+    }
+
+    @Test
+    public void issuerIdentificationNumberPairsWillNotBeFound() throws Exception {
+        final IssuerIdentificationNumber inn = new IssuerIdentificationNumber("101712");
+        final List<Pair<IssuerIdentificationNumber, Bank>> pairs = service.findBanksByIssuerIdentificationNumbers(Collections.singletonList(inn));
+        Assert.assertTrue(pairs != null);
+        Assert.assertTrue(pairs.size() == 1);
+        Assert.assertEquals("101712", pairs.get(0).getValue0().getNumber());
+        Assert.assertTrue(pairs.get(0).getValue1() == null);
     }
 }
